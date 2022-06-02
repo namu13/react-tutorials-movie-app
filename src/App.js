5858d87f4b210f91b4a518d77053f36d2f2814a0
namu <namun1303@gmail.com>
2022-06-02 22:36:41 +0900

    react coin converter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [currentCoin, setCurrentCoin] = useState({});
+  const [inputValue, setInputValue] = useState("");
   useEffect(() => {
     const fetchData = async () => {
       const coinData = await (
@@ -10,20 +12,49 @@ function App() {
       ).json();
       setLoading(false);
       setCoins(coinData);
+      console.log(coinData);
     };
     fetchData();
   }, []);
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li key={coin.id}>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The Coin converter {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select
+          onChange={(event) => {
+            console.log(event.target.dataset);
+            setCurrentCoin(event.target);
+          }}
+        >
+          <option>------Select coin------</option>
+          {coins.map((coin) => (
+            <option key={coin.id} data-currentcoin={coin}>
+              {coin.name} ({coin.symbol})
+            </option>
+          ))}
+        </select>
+      )}
+      <hr></hr>
+      <input
+        type="number"
+        name="usd"
+        id="usd"
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+      ></input>
+      <label htmlFor="usd"> USD</label>
+      <input
+        type="number"
+        name={currentCoin.symbol}
+        id={currentCoin.symbol}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+      ></input>
+      <label htmlFor={currentCoin.symbol}> {currentCoin.symbol}</label>
     </div>
   );
 }
